bc62af71b28ff63e923ec0806d3f4ab4b3171624
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-06-29 01:32:46 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+"""
+This code is part of 'Snapy Chatbot' project
+Copyright (c) 2025, Aymen Brahim Djelloul, All rights reserved.
+Use of this source code is governed by MIT License.
+
+@author : Aymen Brahim Djelloul
+version : 0.2
+date : 25.06.2025
+license : MIT License
+
+"""
+
+# IMPORTS
+from snapy.cli import main
+
+if __name__ == "__main__":
+    main()
